b84ebff09830308b507f9feaa7c36ad2fb085247
Fritz Elfert <fritz@fritz-elfert.de>
2016-02-21 14:29:05 +0100

    Initial commit

---
+++ src/main/java/org/opendataspace/improvedjerseylogging/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Improved Jersey Request & Response Filtering support & utility classes.
+ *
+ * These classes filter the body content similar to apache commons wire logging.
+ */
+package org.opendataspace.improvedjerseylogging;
